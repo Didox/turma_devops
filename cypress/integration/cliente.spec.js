3f5b9d26ea8a5eb52bf50aa4d266f6eee392cb89
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-07-17 12:18:16 -0300

    versionando com BDD

--- cypress/integration/cliente.spec.js
+++ cypress/integration/cliente.spec.js
@@ -5,11 +5,17 @@ context('testar Home Page', () => {
     cy.visit('/')
   })
 
-  it('deveria mostrar a lista de clientes', () => {
-    cy.get('li').should("to.have.length", 4)
+  it('Deve preencher um CPF e verificar como válido', () => {
+    cy.get('input').type('351.455.030-16')
+    cy.get('form button').click()
+    cy.get('.alert-success').should("to.have.length", 1)
+    cy.get('.alert-danger').should("to.have.length", 0)
   })
 
-  it('Deve preencher um CPF', () => {
-    cy.get('input').type('311.796.430-46')
+  it('Deve preencher um CPF e verificar como inválido', () => {
+    cy.get('input').type('351.455.030-26')
+    cy.get('form button').click()
+    cy.get('.alert-success').should("to.have.length", 0)
+    cy.get('.alert-danger').should("to.have.length", 1)
   })
 })

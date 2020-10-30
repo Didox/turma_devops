227a1dffbce2661708fce6a1f149c2f0bc6cf0cc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-10-30 11:26:27 -0300

    atualização em testes

--- cypress/integration/cliente.spec.js
+++ cypress/integration/cliente.spec.js
@@ -5,9 +5,9 @@ context('testar Home Page', () => {
 
   it('Deve preencher um CPF e verificar como válido', () => {
     cy.get('input').type('351.455.030-16')
-    //cy.wait(1000)
+    cy.wait(1000)
     cy.get('form button').click()
-    //cy.wait(1000)
+    cy.wait(1000)
     cy.get('.alert-success').should("to.have.length", 1)
     cy.get('.alert-danger').should("to.have.length", 0)
     //cy.wait(1000)
@@ -15,11 +15,11 @@ context('testar Home Page', () => {
 
   it('Deve preencher um CPF e verificar como inválido', () => {
     cy.get('input').type('351.455.030-26')
-    //cy.wait(1000)
+    cy.wait(1000)
     cy.get('form button').click()
-    //cy.wait(1000)
+    cy.wait(1000)
     cy.get('.alert-success').should("to.have.length", 0)
     cy.get('.alert-danger').should("to.have.length", 1)
-    //cy.wait(1000)
+    cy.wait(1000)
   })
 })
